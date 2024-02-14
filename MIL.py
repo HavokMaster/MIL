a3d4b4bc4102db94ed7524289a7fbfe670da33c8
HavokMaster <kevinsanthoshzundal@gmail.com>
2024-02-14 22:09:59 +0530

    initial commit

---
+++ MIL.py
@@ -0,0 +1,36 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pickle
+
+def loadImages(imageDir = 'images', size=False, serialize=False, bgr2rgb=False, dumpName="images"):
+    classes = os.listdir(imageDir)
+    images = []
+    labels = []
+    
+    print(f"{len(classes)} Total class/classes detected!")
+    for idx, classLabel in enumerate(classes):
+        imgNo = 0
+        for img in os.listdir(f'{imageDir}/{classLabel}'):
+            image = cv2.imread(f'{imageDir}/{classLabel}/'+img)
+            if bgr2rgb: image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            if size != False: image = cv2.resize(image, size)
+            images.append(image)
+            labels.append(idx)
+            imgNo += 1
+        if imgNo == 0: print(f"No images loaded from {classLabel}")
+        else: print(f"Loaded {imgNo} image(s) from {classLabel}")
+    if serialize:
+        with open(f'{dumpName}.pkl', 'wb') as file:
+            pickle.dump({"Images":images, "Labels":labels}, file)
+    return [images, labels]
+
+def loadFromDump(dumpName='images.pkl'):
+    with open(dumpName, 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+def getClasses(imageDir='images'):
+    classes = os.listdir(imageDir)
+    return classes
