4f7edf703934a56520000a781eb43775553521c2
HavokMaster <53501215+HavokMaster@users.noreply.github.com>
2024-02-14 23:59:10 +0530

    Update MIL.py

--- MIL.py
+++ MIL.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import pickle
 
-def loadImages(imageDir = 'images', size=False, serialize=False, bgr2rgb=False, dumpName="images"):
+def loadImages(imageDir = 'images', size=False, serialize=False, bgr2rgb=False, dumpName="images", maxSize=2000, normalize=False):
     classes = os.listdir(imageDir)
     images = []
     labels = []
@@ -13,6 +13,7 @@ def loadImages(imageDir = 'images', size=False, serialize=False, bgr2rgb=False, 
     for idx, classLabel in enumerate(classes):
         imgNo = 0
         for img in os.listdir(f'{imageDir}/{classLabel}'):
+            if imgNo >= maxSize: break
             image = cv2.imread(f'{imageDir}/{classLabel}/'+img)
             if bgr2rgb: image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             if size != False: image = cv2.resize(image, size)
@@ -24,6 +25,9 @@ def loadImages(imageDir = 'images', size=False, serialize=False, bgr2rgb=False, 
     if serialize:
         with open(f'{dumpName}.pkl', 'wb') as file:
             pickle.dump({"Images":images, "Labels":labels}, file)
+    images = np.array(images)
+    labels = np.array(labels)
+    if normalize: images = images.astype('float32')/255.0
     return [images, labels]
 
 def loadFromDump(dumpName='images.pkl'):
